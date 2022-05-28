918b8b94be0468dfa1d94f1bb64e73740b68b61a
shakaknight <ananddubey016@gmail.com>
2022-05-28 20:41:40 +0530

    mongo update

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -1,9 +1,9 @@
 // This file will handle connection logic to the MongoDB database
-
+// mongodb://localhost:27017/TaskManager
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TaskManager', { useNewUrlParser: true }).then(() => {
+mongoose.connect('mongodb+srv://anand:Rediffmail2004@cluster0.ucicz.mongodb.net/major?retryWrites=true&w=majority', { useNewUrlParser: true }).then(() => {
     console.log("Connected to MongoDB successfully :)");
 }).catch((e) => {
     console.log("Error while attempting to connect to MongoDB");
